c01049926ae9da3bc11bb69a0e9b383b4d6378d6
vainall <vaishunalla@yahoo.com>
2022-07-20 09:24:51 -0500

    button components finished

---
+++ src/components/Button/Button.js
@@ -0,0 +1,33 @@
+import './Button.css';
+
+//button sizes: sm, med, lg, xl
+
+
+const SIZES = [
+    'small',
+    'med',
+    'large',
+    'xlarge'
+]
+
+export const Button = ({ 
+    color1, 
+    color2, 
+    children, 
+    onClick, 
+    size }) => {
+
+    const checkButtonSize = SIZES.includes(size) ? size : SIZES[1];
+
+    return (
+
+        <button 
+            onClick={onClick}
+            style={{background: `linear-gradient(90deg, ${color1},${color2})`}}
+            className={`btn ${checkButtonSize}`}>
+                {children}
+        </button>
+    )
+}
+
+export default Button;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Button} from "../Button";
+import {Button} from "../Button/Button.js";
+
+import discordWhite from '../../assets/socialMedia/discordWhite.png';
 
 function Home(props) {
     return (
         <div>
             <h1 style={{color:'white'}}>This is the home page</h1>
-            <Button 
-            color={'linear-gradient(45deg, blue, red)'} 
-            text={'this is a button'} 
-            onClick={console.log('hi')} 
-        />
+            {/* <Button 
+            color1={'#FF005C'}
+            color2={'#4004C0'}
+            children={<img src={discordWhite} style={{maxWidth:30}}></img>} 
+            onClick={alert("button has been clicked")} 
+            size={'xlarge'}
+        /> */}
         </div>
     );
 }
