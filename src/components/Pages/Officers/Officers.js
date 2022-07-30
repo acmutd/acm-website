866d19ca73ea3ba75922b11d559a2e135f994a71
vainall <vaishunalla@yahoo.com>
2022-07-30 09:47:32 -0500

    fixed changes

--- src/components/Pages/Officers/Officers.js
+++ src/components/Pages/Officers/Officers.js
@@ -1,20 +1,10 @@
 import React from 'react';
 
-// function Officers(props) {
-//     return (
-//         <div>
-//             <h1 style={{color:'white'}}>This is the officers page</h1>
-//         </div>
-//     );
-// }
-
 function Officers(props) {
     return (
-        <h1>This is the Officers Page</h1>
+        <div>
+            <h1 style={{color:'white'}}>This is the officers page</h1>
+        </div>
     );
 }
-
-
-
-
 export default Officers;
