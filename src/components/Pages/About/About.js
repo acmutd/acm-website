5b1518ca74516f5761bcc70ecad5b244c9b81b89
vainall <vaishunalla@yahoo.com>
2022-08-04 09:20:35 -0700

    about page changes

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import aboutImage1 from '../../../assets/aboutImage1.png';
+import '../About/About.css';
+import '../Style.css';
+import projectLogoWords from '../../../assets/projects-logo-words-white.png';
 
 function About(props) {
     return (
         <div>
-            <h1 style={{color:'white'}}>This is the about page</h1>
+            <div className='headerContainer'>
+                <img className='headerPhoto' src={aboutImage1}></img>
+
+                <div className='textContainer'>
+
+                    <div className='headingContainer'>
+                        <h1 className='header' style={{ lineHeight: '1.2' }}>we're just a group of people</h1>
+                    </div>
+
+                    <div className='subheadingContainer'>
+                        <h2 className='subheading'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div className='divisionText'>
+
+                <h1 className='header' style={{ textAlign: 'center', marginTop: '7%' }}>Divisions</h1>
+
+                <h2 className='subheading-two' style={{ textAlign: 'center', width: '50%'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</h2>
+            </div>
+
+            <div className='divisionGrid'>
+                <div className='item1' style={{backgroundColor:'red'}}>
+                    <h3>1</h3>
+                    {/* <img className='logo' src={projectLogoWords} style={{backgroundColor:'red'}}></img> */}
+                </div>
+
+            </div>
+
+
+
+
         </div>
     );
 }
