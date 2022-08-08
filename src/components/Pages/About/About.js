3f007c378a0a53838a18456337ae5fcd4a893893
unknown <pranitd23@gmail.com>
2022-08-08 14:19:36 -0500

    grid without acm logo pics

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import aboutImage1 from '../../../assets/aboutImage1.png';
+import projects from "../../../assets/projects.png"
+import dev from "../../../assets/dev.png"
+import diversity from "../../../assets/diversity.png"
+import industry from "../../../assets/industry.png"
+import media from "../../../assets/media.png"
+import research from "../../../assets/research.png"
 import '../About/About.css';
 import '../Style.css';
 import projectLogoWords from '../../../assets/projects-logo-words-white.png';
@@ -27,15 +33,91 @@ function About(props) {
                 <h1 className='header' style={{ textAlign: 'center', marginTop: '7%' }}>Divisions</h1>
 
                 <h2 className='subheading-two' style={{ textAlign: 'center', width: '50%'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</h2>
+              
             </div>
 
+            
             <div className='divisionGrid'>
-                <div className='item1' style={{backgroundColor:'red'}}>
-                    <h3>1</h3>
-                    {/* <img className='logo' src={projectLogoWords} style={{backgroundColor:'red'}}></img> */}
+                <div className='item1' >
+                {/* <img classname="pic item1" src={projects}></img> */}
+
+                </div>
+                <div className='item1' >
+                {/* <h3>1</h3> */}
+
+                </div>
+                <div className='item1' >
+                
+
+                </div>
+                <div className='item1' >
+                
+
+                </div>
+                <div className='item1' >
+                
+
+                </div>
+                <div className='item1' >
+                
+
+                </div>
+                <div className='item1' >
+                
+
+                </div>
+                <div className='item1' >
+                
+
                 </div>
 
+                {/* <div className='item1' >
+                <h3>9</h3>
+                
+
+                </div>
+                <div className='item1' >
+                <h3>10</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>11</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>12</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>13</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>14</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>15</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>16</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>17</h3>
+
+                </div>
+                <div className='item1' >
+                <h3>18</h3>
+
+                </div>
+                */}
+               
+               
+
             </div>
+
 
 
 
