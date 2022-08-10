211e8805a7972450a761ab4cd184d8d2134a9c3a
vainall <vaishunalla@yahoo.com>
2022-08-10 08:01:43 -0700

    fixed background circle

--- src/components/Pages/Test.js
+++ src/components/Pages/Test.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Test.css';
-import projects from "../../assets/projects.png"
 
 export const Test = ({}) => {
   return (
-      <div className='body'>
-       <img src={projects} className="pic"></img>
-      </div>
-
+    <div className='body'>
+    <div className='circle'>
+      <span></span>
+    </div>
+  </div>
     
   )
 }
