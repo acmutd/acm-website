22f3dc23d1affa5587b6d7406a25255b4b23c084
Neha Thomas <nehamol@gmail.com>
2022-08-12 12:57:08 -0500

    reactive scrollbar and scroll jumps

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import Home from './components/Pages/Home/Home';
-import About from './components/Pages/About/About';
-import Apply from './components/Pages/Apply/Apply';
-import Contact from './components/Pages/Contact/Contact';
-import Events from './components/Pages/Events/Events';
-import Officers from './components/Pages/Officers/Officers';
-import Projects from './components/Pages/Projects'
-import Test from './components/Pages/Test';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Pages/Home/Home";
+import About from "./components/Pages/About/About";
+import Apply from "./components/Pages/Apply/Apply";
+import Contact from "./components/Pages/Contact/Contact";
+import Events from "./components/Pages/Events/Events";
+import Officers from "./components/Pages/Officers/Officers";
+import Projects from "./components/Pages/Projects";
+import Test from "./components/Pages/Test";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
 
       {/* just change this to switch between pages */}
 
       {/* Projects Page shows the side bar */}
-      <About/>
+      <Projects />
 
       {/* Test Page shows the background moving circles animation */}
       {/* <Test></Test> */}
       {/* <Officers/> */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/Scrollbar/Scrollbar.js
+++ src/components/Scrollbar/Scrollbar.js
@@ -1,9 +1,126 @@
-import React from 'react';
-import './Scrollbar.css';
+import React, { useEffect, useState, useRef } from "react";
+import "./Scrollbar.css";
 
-export default function Scrollbar() {
-    return (
-         /*} <div className='wrapper'>
+export default function Scrollbar(props) {
+  const binaryArray = [
+    "0",
+    "1",
+    "1",
+    "0",
+    "0",
+    "0",
+    "1",
+    "0",
+    "1",
+    "1",
+    "0",
+    "0",
+    "0",
+    "1",
+    "1",
+    "0",
+    "1",
+    "1",
+    "0",
+    "1",
+    "1",
+    "0",
+    "1",
+    "0",
+    "0",
+    "1",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "1",
+    "1",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "1",
+    "1",
+    "0",
+    "1",
+    "0",
+    "0",
+    "0",
+    "1",
+    "1",
+    "0",
+    "0",
+    "1",
+    "0",
+    "0",
+    "/",
+  ];
+
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    setHeight(document.documentElement.scrollHeight - 110);
+  }, []);
+
+  var n = Math.round(height / 8);
+
+  let binary = new Array(n);
+  for (let i = 0; i < n; i++) {
+    binary[i] = binaryArray[i % 56];
+  }
+
+  const renderBinary = binary.map((number) => (
+    <li className="bin-text" key={number}>
+      {number}
+    </li>
+  ));
+
+  const scrollToSection = (elementRef) => {
+    elementRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="scroll-bar">
+      <div className="fixed-c">
+        <ul className="fixed-c-items">
+          <li
+            ref={props.refs.directors}
+            onClick={() => scrollToSection(props.refs.directors)}
+          >
+            directors
+          </li>
+          <li
+            ref={props.refs.experience}
+            onClick={() => scrollToSection(props.refs.experience)}
+          >
+            experience
+          </li>
+          <li
+            ref={props.refs.projects}
+            onClick={() => scrollToSection(props.refs.projects)}
+          >
+            winning projects
+          </li>
+          <li
+            ref={props.refs.faq}
+            onClick={() => scrollToSection(props.refs.faq)}
+          >
+            faq
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>{renderBinary}</ul>
+      </div>
+    </div>
+
+    /*} <div className='wrapper'>
 
             <div className='main'></div>
 
@@ -22,28 +139,5 @@ export default function Scrollbar() {
 
 
     </div>*/
-
-      <div className='scroll-bar'>
-
-          <div className='fixed-c'>
-          <ul className='fixed-c-items'>
-                    <li>directors</li>
-                    <li>experience</li>
-                    <li>winning projects</li>
-                    <li>faq</li>
-                </ul>
-          </div>
-
-        
-          <div className='bin-text'>
-                01100001011000110110110100100000011101010111010001100100/01100001011000110110110100100000011101010111010001100100/01100001011000110110110100100000011101010111010001100100/01100001011000110110110100100000011101010111010001100100/01100001011000110110110100100000011101010111010001100100/01100001011000110110110100100000011101010111010001100100/01100001011000110110110100100000011101010111010001100100/0110000101100011011011010
-            </div>
-
-
-      </div>
-    
-
-
-    )
+  );
 }
-
