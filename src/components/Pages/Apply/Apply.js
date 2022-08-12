5bf4a83b95f35a4c223513d59e1e89006bedb13b
Nhi Huynh <nhih103@gmail.com>
2022-08-12 04:17:57 -0500

    pasted apply.js

--- src/components/Pages/Apply/Apply.js
+++ src/components/Pages/Apply/Apply.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import './Apply.css';
+import img1 from '../../assets/ApplyPageImage1.png';
+import projectsButton from '../../assets/projectsButton.png';
+import tipButton from '../../assets/tipButton.png';
+import researchButton from '../../assets/researchButton.png';
+import mentorButton from '../../assets/mentorButton.png';
 
-function Apply(props) {
-    return (
-        <div>
-            <h1 style={{color:'white'}}>This is the apply page</h1>
+
+export default function Apply() {
+  return (
+    <div className='format'>
+
+        <div className='image-format'>
+
+          <div>
+            <div className='card' style= {{ height: "100%", width: "400%" }}>   
+              <h1 className='header'>apply</h1>
+              <h2 className='subheading1'>
+              Become a part of ACM today by applying to one of our semester long programs or to be an officer!
+              </h2>
+              <h2 className='subheading1'>
+              All open applications can be found in our member portal at portal.acmutd.co/applications' 
+              </h2>
+            
+            </div>
+    
+          </div>
+          
+          <img className='apply-img'src={img1} alt='group-photo1'/>
+
         </div>
-    );
-}
 
-export default Apply;
+        <div className='body-format'>
+
+          <h2 className= 'subheading2'>
+          our programs
+          </h2>
+
+          <div className= 'button-format'>
+            <h1 className = 'body1'>
+            ACM offers a multitude of programs all targetted towards helping students get a start on their career
+            </h1>
+            <h2 className= 'button-temp'>
+            button here
+            </h2>
+          </div>
+
+          <div className='programs-format'>
+          <img className='program-button'src={projectsButton} alt='group-photo1'/>
+          <img className='program-button'src={tipButton} alt='group-photo1'/>
+          </div>
+
+          <div className='programs-format'>
+          <img className='program-button'src={researchButton} alt='group-photo1'/>
+          <img className='program-button'src={mentorButton} alt='group-photo1'/>
+          </div>
+
+        </div>
+    </div>
+  )
+}
